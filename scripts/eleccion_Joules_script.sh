3235932e85926893cd1c737e2ebed511cb406f20
MrHeisenberg23 <cristiansainzdiego@gmail.com>
2020-03-08 14:44:18 +0100

    Script a falta de recibir bien las señales e incrementar el contador

--- scripts/eleccion_Joules_script.sh
+++ scripts/eleccion_Joules_script.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+trap contador=$((contador+1)) SIGUSR1
 if [ -f "auxiliar1" ]; then
 	rm auxiliar*
 	rm aux
 fi
 muestras=$1
+contador=0
 touch aux
+sortpid=$$
+echo "PID de la terminal original: $sortpid"
 for i in `seq 1 1 $muestras`; do
 	touch auxiliar$i
-	./volcado.sh auxiliar$i
+	./volcado.sh auxiliar$i $sortpid
 done
-sleep 11
+while [ $contador -ne $muestras ]; do
+	echo "esperando finalizacion: contador a $contador."
+	sleep 1
+done
+echo "Tareas finalizadas"
 ./columna_Joules.sh
--- scripts/volcado.sh
+++ scripts/volcado.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gnome-terminal --tab --title="Script" --working-directory="/home/cristian/p2_AIC/scripts" -- /bin/sh -c "pwd; do perf stat -e power/energy-cores/ stress -c 1 -t 10 2> $1; grep Joules $1 >> aux"
+gnome-terminal --tab --title="Script" --working-directory="/home/cristian/p2_AIC/scripts" -- /bin/sh -c "pwd; do perf stat -e power/energy-cores/ stress -c 1 -t 10 2> $1; grep Joules $1 >> aux; kill -SIGUSR1 $2"
